2a887de8d70e284606e4b50fef784bb73dd2aa64
alexoff <davydovaleksey932@gmail.com>
2021-04-12 08:44:29 +1000

    working version

---
+++ tests/tests.cpp
@@ -0,0 +1,47 @@
+#include "gtest/gtest.h"
+#include "../src/queue.h"
+
+using namespace std;
+
+TEST(test,SimpleTestAdd){
+    auto queue = Queue(4);
+    queue.add(Priority::third, 5);
+    queue.add(Priority::third, 5);
+    queue.add(Priority::third, 5);
+    queue.add(Priority::third, 5);
+    int code = queue.add(Priority::third, 5);
+    ASSERT_EQ(code, -1);
+}
+
+TEST(test,SimpleTestPop){
+    auto queue = Queue(4);
+    queue.add(Priority::third, 5);
+    queue.add(Priority::third, 5);
+    queue.add(Priority::third, 5);
+    queue.add(Priority::third, 5);
+    queue.pop();
+    queue.pop();
+    queue.pop();
+    queue.pop();
+    ASSERT_EQ(queue.getLength(), 0);
+}
+
+TEST(test,SimpleTestCopy){
+    auto queue = Queue(4);
+    queue.add(Priority::third, 5);
+    queue.add(Priority::third, 5);
+    queue.add(Priority::third, 5);
+    queue.add(Priority::third, 5);
+    Queue queueCopy = Queue(3);
+
+    Queue queueCopy2 = Queue(2);
+
+    queueCopy = queueCopy2 = queue;
+
+    ASSERT_EQ(queueCopy.getLength(), 2);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
